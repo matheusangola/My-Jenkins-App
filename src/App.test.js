a07e4b3a8ed3f159e4e0f8bf5f422ed65513abab
Matheus <matheusangola97@gmail.com>
2025-03-18 13:54:19 -0600

    Final Commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn', () => {
   render(<App />);
-  const linkElement = screen.getByText(/My Jenkins App/i);
+  const linkElement = screen.getByText(/Matheus Alves/i);
   expect(linkElement).toBeInTheDocument();
 });
